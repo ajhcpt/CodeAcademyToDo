a2156084d621e1c5f133ff44a5d30ac45572f53e
andrew <andrew.henderson79@gmail.com>
2014-02-10 21:28:16 +0200

    Rework on Todo

--- src/main/java/ag/codeacademy/todo/CodeAcademyToDo/App.java
+++ src/main/java/ag/codeacademy/todo/CodeAcademyToDo/App.java
@@ -1,13 +1,33 @@
 package ag.codeacademy.todo.CodeAcademyToDo;
 
-/**
- * Hello world!
- *
- */
+import java.io.IOException;
+
+import ag.codeacademy.todo.services.ReadToDoFromFile;
+
 public class App {
+	//public static ArrayList<ToDo> toDoList;
 	
 	public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+		ToDoBoard toDoBoard = new ToDoBoard(ReadToDoFromFile.ReadFromFile("C:\\downloads\\todos.txt"));
+				
+		System.out.println("Welcome to ToDo application!");
+		System.out.println("Please select an option.");
+		System.out.println("1. List all ToDo's");
+		System.out.println("2. Add a new ToDo");
+		System.out.println("3. Remove a ToDo");
+		System.out.println("4. Find a ToDo");
+		System.out.print("> ");
+		try {
+			int menuOption = System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for (ToDo toDo : toDoBoard.gettoDoList()) {
+			System.out.println(toDo.getDescription());
+		}
+		
     }
 
 	
--- src/main/java/ag/codeacademy/todo/CodeAcademyToDo/CategoryList.java
+++
@@ -1,7 +0,0 @@
-package ag.codeacademy.todo.CodeAcademyToDo;
-
-import java.util.List;
-
-public class CategoryList {
-	private List CategoryList;
-}
