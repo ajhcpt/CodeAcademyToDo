fb624f1a85e2fc46f21820182954b2f2a893d36c
Andrew Henderson <andrew.henderson@allangray.co.za>
2014-02-28 09:48:07 +0200

    Added Reader interface

---
+++ src/main/java/ag/codeacademy/interfaces/IToDoReader.java
@@ -0,0 +1,9 @@
+package ag.codeacademy.interfaces;
+
+import java.util.List;
+import ag.codeacademy.todo.CodeAcademyToDo.ToDo;
+
+public interface IToDoReader {
+	public List<ToDo> Read();
+
+}
--- src/main/java/ag/codeacademy/todo/services/ToDoFileReader.java
+++ src/main/java/ag/codeacademy/todo/services/ToDoFileReader.java
@@ -7,9 +7,40 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import ag.codeacademy.interfaces.IToDoReader;
 import ag.codeacademy.todo.CodeAcademyToDo.ToDo;
 
-public class ReadToDoFromFile {
+public class ToDoFileReader implements IToDoReader {
+	private BufferedReader reader;
+		
+	ToDoFileReader(BufferedReader reader) {
+		this.reader = reader;
+	}
+	
+	@Override
+	public void Read() {
+		List<ToDo> toDoList = new ArrayList<ToDo>();
+		
+		int numberOfToDos = Integer.parseInt(reader.readLine());
+		for (int i = 0; i < numberOfToDos; i++) {
+			String description = reader.readLine();
+			String detail = reader.readLine();
+			String startDate = reader.readLine();
+			String endDate = reader.readLine();
+			String category = reader.readLine();
+			String importance = reader.readLine();
+			
+			ToDo toDo = new ToDo(description, detail, startDate, endDate, category, importance);
+			toDoList.add(toDo);
+		}
+		return toDoList;
+		
+		
+	}
+	
+	
+	
+	/*
 	public static List<ToDo> ReadFromFile(String fileName) {
 		List<ToDo> toDoList = new ArrayList<ToDo>();
 		
@@ -36,5 +67,5 @@ public class ReadToDoFromFile {
 			e.printStackTrace();
 			return null;
 		}
-	}
+	}*/
 }
